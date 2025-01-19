3cfab9bd48a3fa0bf8731f94da4df925e877deac
anudeeps352 <anudeeps352@gmail.com>
2025-01-19 19:39:10 +0530

    fixed lint issue

--- src/app/pages/shortlist/page.tsx
+++ src/app/pages/shortlist/page.tsx
@@ -4,7 +4,7 @@ import Card from '@/components/card';
 export default function shortlist() {
   return (
     <div className="w-full min-h-screen flex flex-col pt-12 justify-center">
-      <h2 className="text-2xl font-bold mx-14">Shortlisted CV's</h2>
+      <h2 className="text-2xl font-bold mx-14">Shortlisted CV&apos;s</h2>
       <div className="w-[95%] mx-auto flex py-10 flex-col gap-4 ">
         {data.length > 0 ? (
           data.map((item, index) => (
