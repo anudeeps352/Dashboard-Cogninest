42b470c6ff59b70c2a650adcd07f294e16f45032
anudeeps352 <anudeeps352@gmail.com>
2025-01-19 19:33:48 +0530

    fixed typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Sidebar from '@/components/sidebar';
-import UserAvatar from '@/components/useravatar';
+import UserAvatar from '@/components/userAvatar';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
